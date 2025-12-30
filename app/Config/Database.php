b7fa17f41f9961d359858a87d778665abd414fef
Mika <ktrianaa2@uteq.edu.ec>
2025-12-30 15:13:47 -0500

    Ajuste de credenciales

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'          => '',
         'hostname'     => 'localhost',
         'username'     => 'root',
-        'password'     => '123456',
+        'password'     => '1234',
         'database'     => 'tienda',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
